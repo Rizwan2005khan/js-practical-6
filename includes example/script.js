72f9f69abece775da468c67ce6850bd9e39aa709
Rizwan Ullah <rizwanullahicp0306@gmail.com>
2024-07-12 21:14:03 +0500

    Update script.js

--- includes example/script.js
+++ includes example/script.js
@@ -1,10 +1,8 @@
-function checkIncludes() {
-    let str = "Hello";
-    let includesO = str.includes('o');
-    
-    if (includesO) {
-        alert("'Hello' includes 'o'");
-    } else {
-        alert("'Hello' does not include 'o'");
-    }
+let str = "Hello";
+let includesO = str.includes('o');
+
+if (includesO) {
+    alert("'Hello' includes 'o'");
+} else {
+    alert("'Hello' does not include 'o'");
 }
