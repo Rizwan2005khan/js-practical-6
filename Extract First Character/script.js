5127e4265cf5cab72fcd93a33b74a1a7aeff83cd
Rizwan Ullah <rizwanullahicp0306@gmail.com>
2024-07-12 21:09:16 +0500

    Update script.js

--- Extract First Character/script.js
+++ Extract First Character/script.js
@@ -1,6 +1,4 @@
-function extractFirstCharacters() {
-    let str = "Hello World";
-    let first5Characters = str.substring(0, 5); // Extracts characters from index 0 to 4
-    
-    alert("First 5 characters: '" + first5Characters + "'");
-}
+let str = "Hello World";
+let first5Characters = str.substring(0, 5); 
+
+alert("First 5 characters: '" + first5Characters + "'");
