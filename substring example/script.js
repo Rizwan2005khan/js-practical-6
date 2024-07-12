2343595ba9f9f13c37fb53686924e2a60cd3f886
Rizwan Ullah <rizwanullahicp0306@gmail.com>
2024-07-12 21:19:41 +0500

    Update script.js

--- substring example/script.js
+++ substring example/script.js
@@ -1,6 +1,4 @@
-function getSubstring() {
-    let str = "Hello World";
-    let substring = str.substring(5, 11); // Extracts characters from index 5 to 10 (end index is exclusive)
-    
-    alert("Substring from index 5 to 10: '" + substring + "'");
-}
+let str = "Hello World";
+let substring = str.substring(5, 11); 
+
+alert("Substring from index 5 to 10: '" + substring + "'");
