8bd399fb9c81d19656de0c571e5a6b138915c0fc
Rizwan Ullah <rizwanullahicp0306@gmail.com>
2024-07-12 21:15:19 +0500

    Update script.js

--- join array example/script.js
+++ join array example/script.js
@@ -1,6 +1,4 @@
-function joinArray() {
-    let array = ["hello", "world"];
-    let joinedString = array.join(' '); // Join array elements with a space separator
-    
-    alert("Joined string: '" + joinedString + "'");
-}
+let array = ["hello", "world"];
+let joinedString = array.join(' '); 
+
+alert("Joined string: '" + joinedString + "'");
