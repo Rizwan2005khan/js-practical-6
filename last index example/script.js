329ab9d4b28026b2583077dcd6a6a24f6b412fe9
Rizwan Ullah <rizwanullahicp0306@gmail.com>
2024-07-12 21:16:08 +0500

    Update script.js

--- last index example/script.js
+++ last index example/script.js
@@ -1,6 +1,4 @@
-function findLastIndex() {
-    let str = "Hello World World";
-    let lastIndex = str.lastIndexOf('World');
-    
-    alert("Last Index of 'World': " + lastIndex);
-}
+let str = "Hello World World";
+let lastIndex = str.lastIndexOf('World');
+
+alert("Last Index of 'World': " + lastIndex);
