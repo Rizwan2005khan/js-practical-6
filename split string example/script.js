4cd1d54406868341c8b4a417eec47472d6c71822
Rizwan Ullah <rizwanullahicp0306@gmail.com>
2024-07-12 21:17:42 +0500

    Update script.js

--- split string example/script.js
+++ split string example/script.js
@@ -1,7 +1,5 @@
-function splitString() {
-    let str = "hello-world";
-    let array = str.split('-');
-    
-    console.log("Original string: '" + str + "'");
-    console.log("Array after splitting: ", array);
-}
+let str = "hello-world";
+let array = str.split('-');
+
+console.log("Original string: '" + str + "'");
+console.log("Array after splitting: ", array);
