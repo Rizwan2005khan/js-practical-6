d7c2c53507dbcda35506edd0ed6d5f8dabf8dc32
Rizwan Ullah <rizwanullahicp0306@gmail.com>
2024-07-12 21:06:38 +0500

    Update script.js

--- End with examples/script.js
+++ End with examples/script.js
@@ -1,10 +1,8 @@
-function checkEndsWith() {
-    let str = "Hello World";
-    let endsWithWorld = str.endsWith('World');
-    
-    if (endsWithWorld) {
-        alert("'Hello World' ends with 'World'");
-    } else {
-        alert("'Hello World' does not end with 'World'");
-    }
+let str = "Hello World";
+let endsWithWorld = str.endsWith('World');
+
+if (endsWithWorld) {
+    alert("'Hello World' ends with 'World'");
+} else {
+    alert("'Hello World' does not end with 'World'");
 }
